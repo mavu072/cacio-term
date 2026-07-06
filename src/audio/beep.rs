b5c425b22a8f7b2810f398f15a753758919fe601
Avuyile Mgxotshwa <avuyile.mgxotshwa@bluerobot.com>
2026-07-06 03:16:51 +0200

    feat: make actually_beep crate optional in CI

--- src/audio/beep.rs
+++ src/audio/beep.rs
@@ -1,7 +1,13 @@
-use actually_beep::beep_with_hz_and_millis;
-
+// Block will only compile if the beep feature is on
+#[cfg(feature = "beep_with_hz_and_millis")]
 pub fn system_beep() {
-    beep_with_hz_and_millis(2000, 100).unwrap();
+    actually_beep::beep_with_hz_and_millis(2000, 100).unwrap();
+}
+
+// Fallback when running in CI without the feature
+#[cfg(not(feature = "beep_with_hz_and_millis"))]
+pub fn system_beep() {
+    println!("Beep skipped.");
 }
 
 #[cfg(test)]
